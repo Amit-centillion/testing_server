88816b1e4f2d9da37fa0c11970d75033e5c5a928
devcentillion04 <81748155+devcentillion04@users.noreply.github.com>
2022-09-14 16:13:29 +0530

    changes add init

---
+++ server.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+
+var express = require("express");
+var app = express();
+
+app.use(express.json());
+
+app.post("/post/data", function (req, res) {
+  console.log("receiving data...");
+  console.log("body is ", req.body);
+  res.send(req.body);
+});
+
+// your express configuration here
+app.get("/getData", function (req, res) {
+  return res.json("Hello, Secure World");
+});
+
+var httpServer = http.createServer(app);
+
+// For http
+httpServer.listen(8080);
+console.log("server listening on port 8080"); // For https
